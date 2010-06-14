ab760612831bcd1def1eb952f22254e3096c1886
canalpay <canalpayciftci@gmail.com>
2010-06-14 15:10:41 +0300

    DesteDeğerTanımla işlevi eklendi.
    Eklenirken pişti puanının değerinin normal kart puanından ayrı olması gerektiği anlaşıldı.
    Ona göre düzenlemeler yapıldı. Ve DesteDeğerTanımla bazı özel kartları yanlış tanımlıyor.
    Düzeltilmeli.

--- pisti/kart.d
+++ pisti/kart.d
@@ -16,9 +16,14 @@ struct OyunKartı
     
 enum kartPuanı
 {
+    yok          =0,
     tek          =1,
     çift         =2,
     üç           =3,
+}
+enum piştiPuanı
+{
+
     pişti        =10,
     valeliPişti  =20        }
 
@@ -35,6 +40,7 @@ struct DesteDeğer
 {
     OyunKartı kart;
     kartPuanı puan;
+    piştiPuanı pişti;
     oynanmış oynanmış_mı;
     kartDavranışı davranış;
     
@@ -53,4 +59,20 @@ OyunKartı[] OyunKartıTanımla()
     }
     
     return kart;
+}
+///////// Özel kartlar için DesteDeğer Şuanlık hatalı.
+/*
+ * A         : 1 puan x 4 = 4 puan
+ * J         : 1 puan x 4 = 4 puan  //Alır //pişti 20 puan
+ * sinek 2'li: 2 puan x 1 = 2 puan
+ * Karo 10'lu: 3 puan x 1 = 3 puan
+ * 
+ * */
+DesteDeğer[] DesteDeğerTanımla(OyunKartı[] kartlar)
+{
+	DesteDeğer[] değerler;
+	foreach(kart;kartlar){
+		değerler~=DesteDeğer(kart,kartPuanı.yok,piştiPuanı.pişti,oynanmış.hayır,kartDavranışı.normal);
+	}
+	return değerler;
 }
