3a75550fb6ea87dce6e5d4f7c530bad1582d8915
canalpay <canalpayciftci@gmail.com>
2010-06-14 11:09:34 +0300

    Kart.d'ye kart değerini tutacak yapılar eklendi.

--- pisti/kart.d
+++ pisti/kart.d
@@ -1,1 +1,41 @@
 // Kart davranışları ve puanları burada tanımlanacak.
+module kart;
+
+
+struct OyunKağıdı
+{
+	
+	dchar biçim;
+	dchar değer;
+
+	string toString()
+	{
+		return format("%s %s",değer,biçim);
+	}
+}    
+	
+enum kartPuanı
+{
+	tek          =1,
+	çift         =2,
+	üç           =3,
+	pişti        =10,
+	valeliPişti  =20        }
+
+enum kartDavranışı
+{
+	normal,alır  }
+
+enum oynanmış
+{
+	evet,hayır }
+
+
+struct desteDeğer
+{
+	OyunKağıdı kart;
+	kartPuanı puan;
+	oynanmış oynanmış_mı;
+	kartDavranışı davranış;
+	
+}
