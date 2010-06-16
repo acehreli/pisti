0b0988f9602c110c23d4d2ec24a05b4c579a1c47
canalpay <canalpayciftci@gmail.com>
2010-06-16 14:10:19 +0300

    yereOyunKartıAt adı dörtOyunKartıAl oldu.

--- pisti/görevli.d
+++ pisti/görevli.d
@@ -1,7 +1,8 @@
 // Puanları sayacak. Puanarı oyuncuya aktaracak. Oyuncuya kart çektircek...
 import kart;
-
-OyunKartı[] yereOyunKartıAt(ref OyunKartı[] kartlar)
+// dört tane oyun kartı alır. Aldığı 4 Oyun kartının dizisini döndürür
+// ve bu 4 oyun kartını OyunKartı referans dizisinden çıkarır.
+OyunKartı[] dörtOyunKartıAl(ref OyunKartı[] kartlar)
 {
     OyunKartı[] atılacakKartlar=kartlar[0..4].dup;
     kartlar = kartlar[4..$];
@@ -11,7 +12,7 @@ unittest
 {
     auto kart = OyunKartıTanımla();
     auto kart2 = kart.dup;
-    auto kart3 = yereOyunKartıAt(kart);
+    auto kart3 = dörtOyunKartıAl(kart);
     assert(kart2[0..4]==kart3);
     assert(kart2[4..$]==kart);
 }
