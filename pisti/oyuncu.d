6f49297cea5075481ab513b4be22601987fe7f31
canalpay <canalpayciftci@gmail.com>
2010-06-21 15:37:22 +0300

    oyuncu.d'ye gelişiGüzelSeç işlevi eklendi.

--- pisti/oyuncu.d
+++ pisti/oyuncu.d
@@ -1,2 +1,11 @@
 // Hangi kartı atacağını belirleyecek.(Yapayzeka : Oyuncu)
-import görevli;
+import kart;
+import std.random;
+
+OyunKartı gelişiGüzelSeç(ref OyunKartı[] kartlar)
+{
+    int sayı  = uniform(0,kartlar.length);
+    OyunKartı   seçilenKart = kartlar[sayı];
+    kartlar   = kartlar[0..sayı].dup ~ kartlar[sayı+1..$].dup;
+    return seçilenKart;
+}
